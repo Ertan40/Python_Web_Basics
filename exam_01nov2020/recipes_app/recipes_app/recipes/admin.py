56a752959754db2874eb0f3cfea26a6a6f93c6f1
Ertan Donmez <ertandonmez1@gmail.com>
2023-06-15 22:29:29 +0300

    update

--- exam_01nov2020/recipes_app/recipes_app/recipes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from recipes_app.recipes.models import Recipe
-
-
-# Register your models here.
-@admin.register(Recipe)
-class AdminRecipe(admin.ModelAdmin):
-    ...
